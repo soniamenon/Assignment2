cfd29ecf884f91f33d7dc36405000fe601a8ec41
soniamenon <=>
2018-12-13 19:16:20 +0100

    Checkstyle changed - close #6

--- src/main/java/it/unipd/tos/business/BillCreator.java
+++ src/main/java/it/unipd/tos/business/BillCreator.java
@@ -7,7 +7,9 @@ package it.unipd.tos.business;
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BillCreator implements RestaurantBill {
     @Override
@@ -21,11 +23,23 @@ public class BillCreator implements RestaurantBill {
 
     private static double getTotalAmount (List<MenuItem> itemsOrdered) {
         double totalAmount = 0;
+        double totalDiscount = 0;
 
         for(MenuItem i : itemsOrdered) {
             totalAmount += i.getPrice();
         }
 
-        return totalAmount;
+        totalDiscount += pizzaDiscount(itemsOrdered);
+        return totalAmount - totalDiscount;
+    }
+
+    private static List<MenuItem> getFilteredItemList (List<MenuItem> itemsOrdered, MenuItem.ItemType itemType) {
+        return itemsOrdered.stream().filter((menuItem -> menuItem.getItemType() == itemType)).collect(Collectors.toList());
+    }
+
+    private static double pizzaDiscount (List<MenuItem> itemsOrdered) {
+        List<MenuItem> pizzasOrdered = getFilteredItemList(itemsOrdered, MenuItem.ItemType.PIZZE);
+
+        return pizzasOrdered.size()>10 ? pizzasOrdered.stream().min(Comparator.comparing(MenuItem::getPrice)).get().getPrice() : 0;
     }
 }
